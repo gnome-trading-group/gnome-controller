703fdb9ac2924148bc45690e9911b9890413856c
mprey <masonprey7@gmail.com>
2025-06-12 18:12:23 -0400

    fix: comment out local bundling as the prod bundle was picking it up

--- cdk/lib/stacks/frontend-stack.ts
+++ cdk/lib/stacks/frontend-stack.ts
@@ -129,13 +129,14 @@ export class FrontendStack extends cdk.Stack {
         image: cdk.DockerImage.fromRegistry('public.ecr.aws/docker/library/node:18'),
         local: {
           tryBundle(outputDir: string): boolean {
-            try {
-              // If you're running locally, make sure to run `npm run build` in the UI beforehand
-              execSync(`cp -r ${uiPath}/dist/* ${path.join(outputDir)}`)
-            } catch {
-              return false
-            }
-            return true
+            return false;
+            // try {
+            //   // If you're running locally, make sure to run `npm run build` in the UI beforehand
+            //   execSync(`cp -r ${uiPath}/dist/* ${path.join(outputDir)}`)
+            // } catch {
+            //   return false
+            // }
+            // return true
           },
         },
         command: [
@@ -144,8 +145,6 @@ export class FrontendStack extends cdk.Stack {
             `cp .env.${props.stage} .env`,
             'npm ci',
             'npm run build',
-            'echo "HELLO!!!!"',
-            `echo "STAGE: ${props.stage}"`,
             'cp -r dist/* /asset-output',
           ].join(' && ')
         ],
