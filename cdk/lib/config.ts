69802d4f95d66c101cdb75322aa0cb292ec6ad35
mprey <masonprey7@gmail.com>
2025-07-08 10:08:57 -0400

    misc(*): bump orchestrator

--- cdk/lib/config.ts
+++ cdk/lib/config.ts
@@ -21,7 +21,7 @@ export interface ControllerConfig {
 
 const defaultConfig = {
   slackWorkspaceId: "T08K71WNHSR",
-  collectorOrchestratorVersion: "1.0.25",
+  collectorOrchestratorVersion: "1.1.0",
 }
 
 export const CONFIGS: { [stage in Stage]?:  ControllerConfig } = {
